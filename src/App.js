e83b9597563d88cf1684a8e9f63029adfc15e051
Anjali <anjaliguptajnp21@gmail.com>
2024-01-12 16:46:40 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { MainComponent } from "./MainComponent.js";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MainComponent />
+    </div>
+  );
+}
